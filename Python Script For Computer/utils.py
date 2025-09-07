12ae6ce65f449213e42a95d891888577b1249d59
garrethklauer <garrethklauer@gmail.com>
2025-09-07 16:04:56 -0400

    Just added some more to the python scripted and added a folder for the ai

--- Python Script For Computer/utils.py
+++ Python Script For Computer/utils.py
@@ -1,24 +1,33 @@
 import os
+import sys
+import ctypes
 
+ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
 def RunThroughDir():
     os.chdir("c:/")
     Dir = os.listdir()
     
-    index = 0
+    index = -1
     count = 0
-    while index + 1 < len(Dir):
+    while index < len(Dir):
         print(Dir[index])
         index += 1
         count += 1
         current_dir = Dir[index]
-        #for i in range(index):
         try:
             os.chdir("c:/" + str(current_dir) + "/")
+            with os.scandir("c:/" + str(current_dir) + "/") as entries:
+                for entry in entries:
+                    if entry.is_dir() == True:
+                        os.chdir("c:/" + str(current_dir) + "/" + str(entry.name) + "/")
         except (FileNotFoundError, NotADirectoryError, PermissionError) as e:
             print(f"cant open: {current_dir} - {e}")
             index += 1
             continue            
         print(os.listdir("c:/" + str(current_dir) + "/"))
+        
+
+
 
         
 
